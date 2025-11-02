57d87ebf2bca983bbebd16e52c0f975cd9525c9e
maheshlamichhane048@gmail.com <maheshlamichhane048@gmail.com>
2025-11-02 17:19:34 +0545

    Java declaration and access modifiers completed

---
+++ Java Declaration And Access Modifiers/src/Main.java
@@ -0,0 +1,8 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+class Main {
+    public static void main(String[] args) {
+        TestMain obj = new TestMain();
+
+    }
+}
