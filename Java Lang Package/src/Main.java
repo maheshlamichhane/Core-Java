0974272487333690e31aa5994ca5ba058bf6fa36
maheshlamichhane048@gmail.com <maheshlamichhane048@gmail.com>
2025-11-03 23:51:38 +0545

    java lang package completed

---
+++ Java Lang Package/src/Main.java
@@ -0,0 +1,15 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        Object o = new Object();
+
+        String s = new String("mahesh");
+        s.toUpperCase();
+        System.out.println(s);
+
+        String s2 = "hello";
+        s2.toUpperCase();
+
+    }
+}
